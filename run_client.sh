1bf96e971631ac2b411abb9580798e14b9effc8f
hanzy1110 <hanzy1110@github.com>
2023-10-10 10:32:06 -0300

    run script

---
+++ run_client.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -xe
+RTSP_IP="rtsp://@192.168.1.12:554/cam/realmonitor?channel=1&subtype=0"
+PASSWORD="camarasuncosma2020"
+OUTFILE="/home/admintaller/vigilancia.mp4"
+
+cargo run --package client mp4 --url "$RTSP_IP" --username admin --password $PASSWORD $OUTFILE
