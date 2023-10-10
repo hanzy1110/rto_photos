6209085c87ecc4ff5890f58f58bc9e8c8bf1fb4b
hanzy1110 <hanzy1110@github.com>
2023-10-10 15:29:27 -0300

    commited to s3

--- run_client.sh
+++ run_client.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -xe
+source .env.prod
+
 PASSWORD="camarasuncosma2020"
 OUTPUT_DIR="/home/admintaller/FOTOS"
 RTSP_URL="rtsp://admin:${PASSWORD}@192.168.1.12:554/cam/realmonitor?channel=1&subtype=0"
@@ -8,35 +10,34 @@ mkdir -p $OUTPUT_DIR
 NUM_PICTURES=5
 INTERVAL_SECONDS=20
 
-for ((i=1; i<=NUM_PICTURES; i++)); do
+S3_DESTINATION="s3://${S3_BUCKET}/rto-nqn-files"
+
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY"
+aws configure set aws_secret_access_key "$AWS_SECRET_KEY"
+aws configure set region "$AWS_DEFAULT_REGION"
+
+for ((i = 1; i <= NUM_PICTURES; i++)); do
     TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-    OUTPUT_FILE="$OUTPUT_DIR/snapshot_$TIMESTAMP.png"
+    OUTPUT_FILE="$OUTPUT_DIR/snapshot_$TIMESTAMP.raw"
+    OUTPUT_FILE_JPG="$OUTPUT_DIR/snapshot_$TIMESTAMP.jpg"
 
     # Capture a single frame
     ffmpeg -i "$RTSP_URL" -vf "select=eq(pict_type\,I)" -vframes 1 -c:v libx264 -crf 0 "$OUTPUT_FILE"
+    ffmpef -i $OUTPUT_FILE $$OUTPUT_FILE_JPG
 
     echo "Captured picture $i at $TIMESTAMP"
+
+    if [ $? -eq 0 ]; then
+        echo "File uploaded to S3 successfully."
+        rm $OUTPUT_FILE $OUTPUT_FILE_JPG
+    else
+        echo "File upload to S3 failed."
+    fi
 
     if [ $i -lt $NUM_PICTURES ]; then
         sleep $INTERVAL_SECONDS
     fi
 done
-#
-echo "PHOTOS TAKEN"
-#
-# Start capturing the RTSP feed and save it to a file
-# ffmpeg -i "$RTSP_URL" -vf "select=eq(pict_type\,I)" -vframes 1 -c:v libx265 -crf 0 $OUTPUT_FILE
-# # Store the PID of the ffmpeg process
-# FFMPEG_PID=$!
-# # Wait for some time (e.g., 30 seconds) to capture the video
-# sleep 30
 
-# # Kill the ffmpeg process
-# kill "$FFMPEG_PID"
-# # Upload the captured file to the remote server using scp
-# scp "$OUTPUT_FILE" "$REMOTE_SERVER"
-# # Clean up the local file (optional)
-# rm "$OUTPUT_FILE"
-# echo "RTSP capture and upload complete."
-# cargo run --package client info --url "$RTSP_IP" --username admin --password $PASSWORD
-# cargo run --package client mp4 --url "$RTSP_IP" --username admin --password $PASSWORD $OUTFILE
+gzip -c "$OUPUT_DIR" >"FOTOS_${TIMESTAMP}.zip"
+aws s3 cp "FOTOS_${TIMESTAMP}.zip" "$S3_DESTINATION"
---
+++ run_through_ssh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -xe
+
+TALLER_IP=10.0.0.14
+
+ssh admintaller@$TALLER_IP <run_client.sh
