21a23857ea57495ade4b7ad8dc1b362be75bd1a6
hanzy1110 <hanzy1110@github.com>
2023-10-11 12:11:20 -0300

    the lack of experience with cron

--- run_client.sh
+++ run_client.sh
@@ -14,9 +14,9 @@ INTERVAL_SECONDS=30
 
 S3_DESTINATION="s3://rto-nqn-files/FOTOS_TALLER"
 
-aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
-aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
-aws configure set region "us-east-1"
+# /usr/bin/local/aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+# /usr/bin/local/aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+# /usr/bin/local/aws configure set region "us-east-1"
 
 for ((i = 1; i <= NUM_PICTURES; i++)); do
     TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
@@ -38,7 +38,7 @@ done
 
 TAR_OUTPUT="${CAM_IP}_FOTOS_${TIMESTAMP}.tar.gz"
 tar -czvf "$TAR_OUTPUT" "$OUTPUT_DIR"
-aws s3 cp "$TAR_OUTPUT" "$S3_DESTINATION/${TAR_OUTPUT}"
+/usr/bin/local/aws s3 cp "$TAR_OUTPUT" "$S3_DESTINATION/${TAR_OUTPUT}" --config /home/admintaller/.aws/config --credentials /home/admintaller/.aws/credentials
 
 if [ $? -eq 0 ]; then
     echo "File uploaded to S3 successfully."
